c1ff5775dcb90606a5bb690ab182e3d867b9c195
VanNghia <nghiadauvan@gmail.com>
2023-09-24 23:43:21 +0700

    commit

---
+++ BadmintonManagement/models/CUSTOMER.cs
@@ -0,0 +1,31 @@
+namespace BadmintonManagement.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("CUSTOMER")]
+    public partial class CUSTOMER
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public CUSTOMER()
+        {
+            RESERVATIONs = new HashSet<RESERVATION>();
+        }
+
+        [Key]
+        [StringLength(13)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<RESERVATION> RESERVATIONs { get; set; }
+    }
+}
