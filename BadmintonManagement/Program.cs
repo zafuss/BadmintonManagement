6f741ff4b0218dfa438cef5b2549d5c69a944020
zafuss <zafus2103@gmail.com>
2023-09-26 15:57:29 +0700

    added auto create staff account feature

--- BadmintonManagement/Program.cs
+++ BadmintonManagement/Program.cs
@@ -16,7 +16,7 @@ namespace BadmintonManagement
         [STAThread]
         static void Main()
         {
-            UserServices.CreateAdminAccount();
+            UserServices.CreateTestAccounts();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
