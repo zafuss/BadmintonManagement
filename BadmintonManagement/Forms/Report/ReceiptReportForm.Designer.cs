2ef5aab37c755b02a723c051355cfbe237c46bb1
VanNghia <nghiadauvan@gmail.com>
2023-09-27 22:33:55 +0700

    up date form report receipt and income

--- BadmintonManagement/Forms/Report/ReceiptReportForm.Designer.cs
+++ BadmintonManagement/Forms/Report/ReceiptReportForm.Designer.cs
@@ -28,138 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.label2 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.label1 = new System.Windows.Forms.Label();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.txtMonth = new System.Windows.Forms.ComboBox();
-            this.rptReciept = new Microsoft.Reporting.WinForms.ReportViewer();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.panel1.SuspendLayout();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnShowReport = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpMonth = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.dtbStart = new System.Windows.Forms.DateTimePicker();
+            this.rdbDay = new System.Windows.Forms.RadioButton();
+            this.rdbMonth = new System.Windows.Forms.RadioButton();
+            this.rptReceipt = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.groupBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 84);
-            this.panel1.TabIndex = 0;
-            // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.btnShowReport);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Controls.Add(this.txtMonth);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Controls.Add(this.dtpMonth);
+            this.groupBox1.Controls.Add(this.dtpEnd);
+            this.groupBox1.Controls.Add(this.dtbStart);
+            this.groupBox1.Controls.Add(this.rdbDay);
+            this.groupBox1.Controls.Add(this.rdbMonth);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(800, 84);
+            this.groupBox1.Size = new System.Drawing.Size(720, 70);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thời gian";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // dateTimePicker2
+            // btnShowReport
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(489, 48);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker2.TabIndex = 7;
-            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            this.btnShowReport.Location = new System.Drawing.Point(608, 39);
+            this.btnShowReport.Name = "btnShowReport";
+            this.btnShowReport.Size = new System.Drawing.Size(63, 20);
+            this.btnShowReport.TabIndex = 15;
+            this.btnShowReport.Text = "Thống kê";
+            this.btnShowReport.UseVisualStyleBackColor = true;
+            this.btnShowReport.Click += new System.EventHandler(this.btnShowReport_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(428, 50);
+            this.label2.Location = new System.Drawing.Point(394, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Đến Ngày";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(221, 48);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 5;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Đến ngày";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(169, 52);
+            this.label1.Location = new System.Drawing.Point(191, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 13);
-            this.label1.TabIndex = 4;
+            this.label1.TabIndex = 13;
             this.label1.Text = "Từ ngày";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // radioButton2
+            // dtpMonth
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(32, 46);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(121, 17);
-            this.radioButton2.TabIndex = 3;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Thống kê theo ngày";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            this.dtpMonth.CustomFormat = "MM/yyyy";
+            this.dtpMonth.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpMonth.Location = new System.Drawing.Point(243, 16);
+            this.dtpMonth.Name = "dtpMonth";
+            this.dtpMonth.Size = new System.Drawing.Size(137, 20);
+            this.dtpMonth.TabIndex = 12;
             // 
-            // radioButton1
+            // dtpEnd
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(32, 19);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(125, 17);
-            this.radioButton1.TabIndex = 2;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Thống kê theo tháng";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.dtpEnd.CustomFormat = "dd/MM/yyyy";
+            this.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpEnd.Location = new System.Drawing.Point(453, 40);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(137, 20);
+            this.dtpEnd.TabIndex = 11;
             // 
-            // txtMonth
+            // dtbStart
             // 
-            this.txtMonth.FormattingEnabled = true;
-            this.txtMonth.Location = new System.Drawing.Point(172, 15);
-            this.txtMonth.Name = "txtMonth";
-            this.txtMonth.Size = new System.Drawing.Size(121, 21);
-            this.txtMonth.TabIndex = 1;
-            this.txtMonth.SelectedIndexChanged += new System.EventHandler(this.txtMonth_SelectedIndexChanged);
+            this.dtbStart.CustomFormat = "dd/MM/yyyy";
+            this.dtbStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtbStart.Location = new System.Drawing.Point(243, 40);
+            this.dtbStart.Name = "dtbStart";
+            this.dtbStart.Size = new System.Drawing.Size(137, 20);
+            this.dtbStart.TabIndex = 10;
             // 
-            // rptReciept
+            // rdbDay
             // 
-            this.rptReciept.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rptReciept.Location = new System.Drawing.Point(0, 84);
-            this.rptReciept.Name = "rptReciept";
-            this.rptReciept.ServerReport.BearerToken = null;
-            this.rptReciept.Size = new System.Drawing.Size(800, 366);
-            this.rptReciept.TabIndex = 1;
-            this.rptReciept.Load += new System.EventHandler(this.rptReciept_Load);
+            this.rdbDay.AutoSize = true;
+            this.rdbDay.Location = new System.Drawing.Point(52, 43);
+            this.rdbDay.Name = "rdbDay";
+            this.rdbDay.Size = new System.Drawing.Size(121, 17);
+            this.rdbDay.TabIndex = 9;
+            this.rdbDay.TabStop = true;
+            this.rdbDay.Text = "Thống kê theo ngày";
+            this.rdbDay.UseVisualStyleBackColor = true;
+            // 
+            // rdbMonth
+            // 
+            this.rdbMonth.AutoSize = true;
+            this.rdbMonth.Location = new System.Drawing.Point(52, 19);
+            this.rdbMonth.Name = "rdbMonth";
+            this.rdbMonth.Size = new System.Drawing.Size(125, 17);
+            this.rdbMonth.TabIndex = 8;
+            this.rdbMonth.TabStop = true;
+            this.rdbMonth.Text = "Thống kê theo tháng";
+            this.rdbMonth.UseVisualStyleBackColor = true;
+            // 
+            // rptReceipt
+            // 
+            this.rptReceipt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rptReceipt.Location = new System.Drawing.Point(0, 70);
+            this.rptReceipt.Name = "rptReceipt";
+            this.rptReceipt.ServerReport.BearerToken = null;
+            this.rptReceipt.Size = new System.Drawing.Size(720, 380);
+            this.rptReceipt.TabIndex = 1;
             // 
             // ReceiptReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.rptReciept);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(720, 450);
+            this.Controls.Add(this.rptReceipt);
+            this.Controls.Add(this.groupBox1);
             this.Name = "ReceiptReportForm";
             this.Text = "ReceiptForm";
             this.Load += new System.EventHandler(this.ReceiptFormReport_Load);
-            this.panel1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -167,17 +164,17 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private Microsoft.Reporting.WinForms.ReportViewer rptReciept;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private Microsoft.Reporting.WinForms.ReportViewer rptReceipt;
+        private System.Windows.Forms.Button btnShowReport;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.ComboBox txtMonth;
+        private System.Windows.Forms.DateTimePicker dtpMonth;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.DateTimePicker dtbStart;
+        private System.Windows.Forms.RadioButton rdbDay;
+        private System.Windows.Forms.RadioButton rdbMonth;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
     }
 }
---
+++ BadmintonManagement/Forms/Report/ReportForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace BadmintonManagement.Forms.Report
+{
+    partial class ReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReceipt = new System.Windows.Forms.Button();
+            this.btnIncome = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlIncome = new System.Windows.Forms.Panel();
+            this.pnlViewreport = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.pnlIncome.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnReceipt);
+            this.panel1.Controls.Add(this.btnIncome);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(111, 403);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnReceipt
+            // 
+            this.btnReceipt.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReceipt.FlatAppearance.BorderSize = 0;
+            this.btnReceipt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReceipt.Location = new System.Drawing.Point(0, 60);
+            this.btnReceipt.Name = "btnReceipt";
+            this.btnReceipt.Size = new System.Drawing.Size(111, 31);
+            this.btnReceipt.TabIndex = 3;
+            this.btnReceipt.Text = "Thống kê hóa đơn";
+            this.btnReceipt.UseVisualStyleBackColor = true;
+            this.btnReceipt.Click += new System.EventHandler(this.btnReceipt_Click);
+            // 
+            // btnIncome
+            // 
+            this.btnIncome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnIncome.FlatAppearance.BorderSize = 0;
+            this.btnIncome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIncome.Location = new System.Drawing.Point(0, 29);
+            this.btnIncome.Name = "btnIncome";
+            this.btnIncome.Size = new System.Drawing.Size(111, 31);
+            this.btnIncome.TabIndex = 2;
+            this.btnIncome.Text = "Thống kê doanh thu";
+            this.btnIncome.UseVisualStyleBackColor = true;
+            this.btnIncome.Click += new System.EventHandler(this.btnIncome_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(111, 29);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh sách thống kê";
+            // 
+            // pnlIncome
+            // 
+            this.pnlIncome.Controls.Add(this.pnlViewreport);
+            this.pnlIncome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlIncome.Location = new System.Drawing.Point(111, 0);
+            this.pnlIncome.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlIncome.Name = "pnlIncome";
+            this.pnlIncome.Size = new System.Drawing.Size(666, 403);
+            this.pnlIncome.TabIndex = 1;
+            // 
+            // pnlViewreport
+            // 
+            this.pnlViewreport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlViewreport.Location = new System.Drawing.Point(0, 0);
+            this.pnlViewreport.Name = "pnlViewreport";
+            this.pnlViewreport.Size = new System.Drawing.Size(666, 403);
+            this.pnlViewreport.TabIndex = 1;
+            // 
+            // ReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 403);
+            this.Controls.Add(this.pnlIncome);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ReportForm";
+            this.Text = "Thống Kê";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.pnlIncome.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlIncome;
+        private System.Windows.Forms.Button btnIncome;
+        private System.Windows.Forms.Panel pnlViewreport;
+        private System.Windows.Forms.Button btnReceipt;
+    }
+}
