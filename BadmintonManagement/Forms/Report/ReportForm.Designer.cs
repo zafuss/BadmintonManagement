58d697406cb7a627c1fe81d54ce883dc0d5e6467
VanNghia <nghiadauvan@gmail.com>
2023-10-04 15:20:48 +0700

    add reservation report

--- BadmintonManagement/Forms/Report/ReportForm.Designer.cs
+++ BadmintonManagement/Forms/Report/ReportForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCustomerReport = new System.Windows.Forms.Button();
             this.pnlReceiptReport = new System.Windows.Forms.Panel();
             this.btnServiceReceipt = new System.Windows.Forms.Button();
             this.btnCourtReceipt = new System.Windows.Forms.Button();
@@ -36,7 +37,7 @@
             this.btnIncome = new System.Windows.Forms.Button();
             this.pnlIncome = new System.Windows.Forms.Panel();
             this.pnlViewreport = new System.Windows.Forms.Panel();
-            this.btnCustomerReport = new System.Windows.Forms.Button();
+            this.btnReservationReport = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.pnlReceiptReport.SuspendLayout();
             this.pnlIncome.SuspendLayout();
@@ -45,6 +46,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.btnReservationReport);
             this.panel1.Controls.Add(this.btnCustomerReport);
             this.panel1.Controls.Add(this.pnlReceiptReport);
             this.panel1.Controls.Add(this.btnReceipt);
@@ -52,8 +54,23 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(184, 620);
+            this.panel1.Size = new System.Drawing.Size(191, 620);
             this.panel1.TabIndex = 0;
+            // 
+            // btnCustomerReport
+            // 
+            this.btnCustomerReport.BackColor = System.Drawing.Color.LightGray;
+            this.btnCustomerReport.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCustomerReport.FlatAppearance.BorderSize = 0;
+            this.btnCustomerReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCustomerReport.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.btnCustomerReport.Location = new System.Drawing.Point(0, 193);
+            this.btnCustomerReport.Name = "btnCustomerReport";
+            this.btnCustomerReport.Size = new System.Drawing.Size(191, 44);
+            this.btnCustomerReport.TabIndex = 5;
+            this.btnCustomerReport.Text = "Thống kê khách hàng";
+            this.btnCustomerReport.UseVisualStyleBackColor = false;
+            this.btnCustomerReport.Click += new System.EventHandler(this.btnCustomerReport_Click);
             // 
             // pnlReceiptReport
             // 
@@ -63,7 +80,7 @@
             this.pnlReceiptReport.Location = new System.Drawing.Point(0, 96);
             this.pnlReceiptReport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pnlReceiptReport.Name = "pnlReceiptReport";
-            this.pnlReceiptReport.Size = new System.Drawing.Size(184, 97);
+            this.pnlReceiptReport.Size = new System.Drawing.Size(191, 97);
             this.pnlReceiptReport.TabIndex = 4;
             // 
             // btnServiceReceipt
@@ -76,7 +93,7 @@
             this.btnServiceReceipt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnServiceReceipt.Name = "btnServiceReceipt";
             this.btnServiceReceipt.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
-            this.btnServiceReceipt.Size = new System.Drawing.Size(184, 46);
+            this.btnServiceReceipt.Size = new System.Drawing.Size(191, 46);
             this.btnServiceReceipt.TabIndex = 1;
             this.btnServiceReceipt.Text = "Hóa đơn dịch vụ";
             this.btnServiceReceipt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -93,7 +110,7 @@
             this.btnCourtReceipt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCourtReceipt.Name = "btnCourtReceipt";
             this.btnCourtReceipt.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
-            this.btnCourtReceipt.Size = new System.Drawing.Size(184, 46);
+            this.btnCourtReceipt.Size = new System.Drawing.Size(191, 46);
             this.btnCourtReceipt.TabIndex = 0;
             this.btnCourtReceipt.Text = "Hóa đơn sân";
             this.btnCourtReceipt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -110,7 +127,7 @@
             this.btnReceipt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnReceipt.Name = "btnReceipt";
             this.btnReceipt.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
-            this.btnReceipt.Size = new System.Drawing.Size(184, 48);
+            this.btnReceipt.Size = new System.Drawing.Size(191, 48);
             this.btnReceipt.TabIndex = 3;
             this.btnReceipt.Text = "Thống kê hóa đơn";
             this.btnReceipt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -127,7 +144,7 @@
             this.btnIncome.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnIncome.Name = "btnIncome";
             this.btnIncome.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
-            this.btnIncome.Size = new System.Drawing.Size(184, 48);
+            this.btnIncome.Size = new System.Drawing.Size(191, 48);
             this.btnIncome.TabIndex = 2;
             this.btnIncome.Text = "Thống kê doanh thu";
             this.btnIncome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -138,9 +155,9 @@
             // 
             this.pnlIncome.Controls.Add(this.pnlViewreport);
             this.pnlIncome.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlIncome.Location = new System.Drawing.Point(184, 0);
+            this.pnlIncome.Location = new System.Drawing.Point(191, 0);
             this.pnlIncome.Name = "pnlIncome";
-            this.pnlIncome.Size = new System.Drawing.Size(982, 620);
+            this.pnlIncome.Size = new System.Drawing.Size(975, 620);
             this.pnlIncome.TabIndex = 1;
             // 
             // pnlViewreport
@@ -149,23 +166,23 @@
             this.pnlViewreport.Location = new System.Drawing.Point(0, 0);
             this.pnlViewreport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pnlViewreport.Name = "pnlViewreport";
-            this.pnlViewreport.Size = new System.Drawing.Size(982, 620);
+            this.pnlViewreport.Size = new System.Drawing.Size(975, 620);
             this.pnlViewreport.TabIndex = 1;
             // 
-            // btnCustomerReport
+            // btnReservationReport
             // 
-            this.btnCustomerReport.BackColor = System.Drawing.Color.LightGray;
-            this.btnCustomerReport.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnCustomerReport.FlatAppearance.BorderSize = 0;
-            this.btnCustomerReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCustomerReport.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.btnCustomerReport.Location = new System.Drawing.Point(0, 193);
-            this.btnCustomerReport.Name = "btnCustomerReport";
-            this.btnCustomerReport.Size = new System.Drawing.Size(184, 44);
-            this.btnCustomerReport.TabIndex = 5;
-            this.btnCustomerReport.Text = "Thống kê khách hàng";
-            this.btnCustomerReport.UseVisualStyleBackColor = false;
-            this.btnCustomerReport.Click += new System.EventHandler(this.btnCustomerReport_Click);
+            this.btnReservationReport.BackColor = System.Drawing.Color.LightGray;
+            this.btnReservationReport.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReservationReport.FlatAppearance.BorderSize = 0;
+            this.btnReservationReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReservationReport.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.btnReservationReport.Location = new System.Drawing.Point(0, 237);
+            this.btnReservationReport.Name = "btnReservationReport";
+            this.btnReservationReport.Size = new System.Drawing.Size(191, 44);
+            this.btnReservationReport.TabIndex = 6;
+            this.btnReservationReport.Text = "Thống kê phiếu đặt sân";
+            this.btnReservationReport.UseVisualStyleBackColor = false;
+            this.btnReservationReport.Click += new System.EventHandler(this.button1_Click);
             // 
             // ReportForm
             // 
@@ -196,5 +213,6 @@
         private System.Windows.Forms.Button btnServiceReceipt;
         private System.Windows.Forms.Button btnCourtReceipt;
         private System.Windows.Forms.Button btnCustomerReport;
+        private System.Windows.Forms.Button btnReservationReport;
     }
 }
---
+++ BadmintonManagement/Forms/Report/ReservationReport.cs
@@ -0,0 +1,61 @@
+﻿using BadmintonManagement.Models;
+using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BadmintonManagement.Forms.Report
+{
+    
+    public partial class ReservationReport : Form
+    {
+        ModelBadmintonManage context = new ModelBadmintonManage();
+        public ReservationReport()
+        {
+            InitializeComponent();
+        }
+
+        private void ReservationReport_Load(object sender, EventArgs e)
+        {
+            rptReservation.Visible = false;
+        }
+
+        private void btnShowReport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!rdbDay.Checked && !rdbMonth.Checked)
+                    throw new Exception("Vui lòng chọn thời gian thống kê");
+                rptReservation.Visible = true;
+                if (rdbMonth.Checked)
+                {
+                    Microsoft.Reporting.WinForms.ReportParameter[] param = new Microsoft.Reporting.WinForms.ReportParameter[1]
+                    {
+                        new Microsoft.Reporting.WinForms.ReportParameter("StringDate","Tháng "+dtpMonth.Text)
+                    };
+
+                    List<RESERVATION> reservations = context.RESERVATION.ToList();
+                    reservations = reservations.Where(p=>p.CreateDate.Value.ToString("MM/yyyy") == dtpMonth.Text).ToList(); 
+                    rptReservation.LocalReport.ReportPath = "ReservationReport.rdlc";
+                    var soure = new ReportDataSource("DataSetReservation", reservations);
+                    rptReservation.LocalReport.DataSources.Clear();
+                    rptReservation.LocalReport.DataSources.Add(soure);
+                    rptReservation.LocalReport.SetParameters(param);
+                }
+
+
+                this.rptReservation.RefreshReport();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
