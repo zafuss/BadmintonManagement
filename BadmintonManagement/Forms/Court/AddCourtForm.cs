3b229b179088e9d844fd398684c77e0e6a519dbd
vangggggggg <95358830+vangggggggg@users.noreply.github.com>
2023-10-15 12:29:09 +0700

    update check status court

--- BadmintonManagement/Forms/Court/AddCourtForm.cs
+++ BadmintonManagement/Forms/Court/AddCourtForm.cs
@@ -290,6 +290,11 @@ namespace BadmintonManagement.Forms.Court
                 {
                     throw new Exception("Chỉnh sửa sân không thành công!");
                 }
+                else if (new CourtService().CountCourt(cboCourtID.Text) != 0)
+                {
+                    throw new Exception("Chỉnh sửa sân không thành công!");
+                }
+
                 else
                 {
                     DateTime dateTime = DateTime.Now;
